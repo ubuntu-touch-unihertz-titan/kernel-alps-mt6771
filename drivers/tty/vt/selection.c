61af072b8d4cf2c933061b19984b71c25b96d4ad
TheKit <nekit1000@gmail.com>
2024-03-16 14:01:41 +0200

    vt: selection: remove extraneous set_selection definition
    Likely a merge error in the original Android/MediaTek sources

--- drivers/tty/vt/selection.c
+++ drivers/tty/vt/selection.c
@@ -348,18 +348,6 @@ int set_selection(const struct tiocl_selection __user *v, struct tty_struct *tty
 	return ret;
 }
 
-int set_selection(const struct tiocl_selection __user *v,
-	struct tty_struct *tty)
-{
-	int ret;
-
-	console_lock();
-	ret = __set_selection(v, tty);
-	console_unlock();
-
-	return ret;
-}
-
 /* Insert the contents of the selection buffer into the
  * queue of the tty associated with the current console.
  * Invoked by ioctl().
